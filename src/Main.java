c7a3080a2402693808db587ce52fcf957959639d
PavelGunko <g9636406069@yandex.ru>
2022-11-10 22:40:43 +0300

    Corrections 2

--- src/Main.java
+++ src/Main.java
@@ -40,7 +40,7 @@ public class Main {
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
         for (int i = 0; i < products.length; i++) {
-          
+
         }
         int sumdoBonus = 0;
         boolean doBonus = sumdoBonus <= MIN_COST_FOR_BONUS;
@@ -64,7 +64,7 @@ public class Main {
                     sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
 
                 } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб.");
                     sum += prices[i] * counts[i];
                 }
 
